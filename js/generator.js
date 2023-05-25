5e529aaf699437bff28da799a049f9897cefe128
Yaroslav Horievoi <yaroslavgorevoy@gmail.com>
2023-05-25 17:53:56 +0300

    Added move elements to the board. Add generation figure after set up it to the board. Added Modal to the footer

--- js/generator.js
+++ js/generator.js
@@ -8,15 +8,12 @@ function generateCanvas2x2() {
     figure.style.height = '120px';
 
     for (let i = 1; i <= 4; i++) {
-        console.log('SUB')
         let subFigure = document.createElement(`div`);
         subFigure.className = 'sub';
         subFigure.style.width = '60px';
         subFigure.style.height = '60px';
 
         getRandomInt(4);
-
-        console.log(getRandomInt(4))
 
         subFigure.style.background = i === getRandomInt(4) ? 'transparent' : 'red'
 
@@ -36,7 +33,6 @@ function generateCanvas3x2() {
     figure.style.height = '180px';
 
     for (let i = 1; i <= 6; i++) {
-        console.log('SUB')
         let subFigure = document.createElement(`div`);
         subFigure.className = 'sub';
         subFigure.style.width = '60px';
@@ -46,8 +42,6 @@ function generateCanvas3x2() {
 
 
         getRandomInt(4);
-
-        console.log(getRandomInt(4))
 
         figure.appendChild(subFigure)
     }
@@ -68,13 +62,12 @@ function generateCanvas3x3() {
     // figuresList.className = 'big'
     const figure = document.createElement(`div`)
     figure.setAttribute('draggable', 'true')
-    figure.className = 'figure-big'
+    figure.className = 'figure big'
     figure.id = 'figure'
     figure.style.width = '180px';
     figure.style.height = '180px';
 
     for (let i = 1; i <= 9; i++) {
-        console.log('SUB')
         let subFigure = document.createElement(`div`);
         subFigure.className = 'sub';
         subFigure.style.width = '60px';
@@ -85,19 +78,12 @@ function generateCanvas3x3() {
 
         getRandomInt(4);
 
-        console.log(getRandomInt(4))
-
         figure.appendChild(subFigure)
     }
 
     for (let i = 0; i < 5; i++) {
         figure.children[getRandomInt(6)].style.background = 'transparent'
     }
-
-    // if (Math.abs((removableFirst - removableSecond)) !== 1) {
-    //     figure.children[removableFirst].style.background = 'transparent'
-    //     figure.children[removableSecond].style.background = 'transparent'
-    // }
 
     figuresList.append(figure);
 }
@@ -111,33 +97,3 @@ const figuresList = document.getElementById('figures');
 generateCanvas2x2();
 generateCanvas3x2();
 generateCanvas3x3();
-
-const time = setInterval(() => {
-    if (figuresList.children.length < 3) {
-        generateCanvas2x2();
-    }
-}, 100)
-
-const board = document.getElementById('board');
-const figures = document.getElementById('figures');
-const figure = document.getElementById('figure');
-
-board.ondragover = allowDrop;
-
-function allowDrop(event) {
-    event.preventDefault();
-}
-
-figure.ondragstart = drag;
-
-function drag(event) {
-    event.dataTransfer.setData('id', event.target.id)
-}
-
-board.ondrop = drop;
-
-function drop(event) {
-    let itemId = event.dataTransfer.getData('id')
-    console.log(itemId)
-    event.target.append(document.getElementById(itemId))
-}
---
+++ js/move.js
@@ -0,0 +1,53 @@
+let figures = document.querySelectorAll('.figure');
+
+console.log(figures)
+figures.forEach(figure => {
+    move(figure)
+})
+
+function move(figure) {
+    figure.onmousedown = function(event) {
+        figure.style.position = 'absolute';
+        figure.style.zIndex = 1000;
+        figure.style.display = 'grid';
+        figure.style.gridTemplateColumns = '60px 60px';
+
+        document.body.append(figure);
+
+        function moveAt(pageX, pageY) {
+            figure.style.left = pageX - figure.offsetWidth / 2 + 'px';
+            figure.style.top = pageY - figure.offsetHeight / 2 + 'px';
+        }
+
+        moveAt(event.pageX, event.pageY);
+
+        function onMouseMove(event) {
+            moveAt(event.pageX, event.pageY);
+        }
+
+        document.addEventListener('mousemove', onMouseMove);
+
+        figure.onmouseup = function() {
+            document.removeEventListener('mousemove', onMouseMove);
+            figure.onmouseup = null;
+            figure.className = 'setted'
+
+            if (figuresList.children.length < 3) {
+                generateCanvas2x2();
+
+                figures = document.querySelectorAll('.figure');
+
+                figures.forEach((figure) => {
+                    move(figure)
+                })
+                console.log(figures)
+            }
+
+        };
+
+        figure.ondragstart = function() {
+            return false;
+        };
+
+    };
+}
